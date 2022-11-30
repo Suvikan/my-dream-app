f524ec5646eecf8cf42474c62e026be87e1f19ae
Suvikan <sanledell@gmail.com>
2022-11-30 09:53:13 +0200

    change servers components to use back tick template

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -2,7 +2,13 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-servers',
-  templateUrl: './servers.component.html',
+  template: `
+  <section>
+    <h1>Hi! I'm in the ServerS Componenet!</h1>
+    <p>Below this text I have 2 Server Componenets</p>
+    <app-server></app-server>
+    <app-server></app-server>
+</section>`,
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
